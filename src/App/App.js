bd2879b2983208b94a9a84ee1f078a8de163a931
Aleksandr-Yashnik <jashnik.aleksandr@gmail.com>
2024-06-30 20:34:24 +0200

    Добавил все лекции

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+//Lessons
+//import Lesson02 from "../Lessons/Lesson02/Lesson02";
+import Lesson03 from "../Lessons/Lesson03/Lesson03";
+//Homeworks
+// import Homework02 ..
+
+function App() {
+  return (
+    <div className="App">
+      <Lesson03 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+//Lessons
+// import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./Lessons/Lesson03/Lesson03";
+
+//Homeworks
+import Homework02 from "../homeworks/Homework02/Homework02";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Lesson02 /> */}
+       <Homework02 />
+      <Lesson03 />
+    </div>
+  );
+}
+
+export default App;
